ef93b6942cfc86919e1b00016f1a9987ae69a2c8
Martin <martin.lippl@pueo.de>
2021-03-07 11:44:26 +0100

    * Added PyTorch definitions for networks

---
+++ networks_torch.py
@@ -0,0 +1,48 @@
+def seeding(seed=10):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
+
+
+class Actor(nn.Module):
+    def __init__(self, state_size, action_size, fc1_units=256, fc2_units=128):
+        super(Actor, self).__init__()
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state):
+        x = F.elu(self.fc1(state))
+        x = F.elu(self.fc2(x))
+        return F.tanh(self.fc3(x))
+
+
+class Critic(nn.Module):
+    def __init__(self, full_state_size, full_action_size, fcs1_units=256, fc2_units=128):
+        super(Critic, self).__init__()
+        self.fcs1 = nn.Linear(full_state_size, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units+full_action_size, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, full_state, full_action):
+        xs = F.elu(self.fcs1(full_state))
+        x = torch.cat((xs, full_action), dim=1)
+        x = F.elu(self.fc2(x))
+        return self.fc3(x)
